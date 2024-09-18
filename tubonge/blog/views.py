9a1dc01e57c37963c20aa8bbb7755f031d08eb67
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-09-18 11:59:08 +0300

    add the URL patterns and template views for the application allowing for its display

--- tubonge/blog/views.py
+++ tubonge/blog/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import Http404
+from .models import Post
 
 # Create your views here.
+
+# list all the posts published
+def post_list(request):
+    posts = Post.published.all()
+
+    return render (request, 
+                   'blog/post/list.html', 
+                   {'posts':posts},)
+
+# list a aprticular post in detail
+def post_detail(request, id):
+#     try:
+#         posts = Post.published.get(id=id)
+#     except:
+#         raise Http404('Post does not exist')
+    
+#     return render(request,
+#                   'blog/post/detail.html',
+#                   {'posts':posts},)
+    post = get_object_or_404(Post,
+                             id=id,
+                             status=Post.Status.PUBLISHED)
+    
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post':post},)

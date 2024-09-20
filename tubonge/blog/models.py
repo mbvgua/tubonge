e393b025fcdb3f3e4569a17352b525e95c37acac
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-09-20 11:04:20 +0300

    add canonical urls to the posts detail

--- tubonge/blog/models.py
+++ tubonge/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -53,3 +54,8 @@ class Post(models.Model):
     # return human readable name whe queried
     def __str__(self):
         return self.title
+    
+    # using canonical urls
+    def get_absolute_urls(self):
+        return reverse('blog:post_detail',
+                       args=[self.id])

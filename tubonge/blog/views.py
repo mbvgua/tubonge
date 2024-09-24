6d1dd660f9921a5c32b3679b9e8e995d7a111616
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-09-24 05:49:33 +0300

    add pagination to the blog posts
    paginate the blogs, whereby user sees a limit of 3 posts per page. Also implement error handling, such that is a request for a page out of rang or a page number is not an integer it will be handled correctly

--- tubonge/blog/views.py
+++ tubonge/blog/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 # Create your views here.
 
 # list all the posts published
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # if page request is not an integer return the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page request is out of range, return last page
+        posts = paginator.page(paginator.num_pages) 
 
     return render (request, 
                    'blog/post/list.html', 
